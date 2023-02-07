d5474fd75e58750caa4c4d5791c0838ec7364fda
Pieter Jan Bonestroo <pjbonestroo@hotmail.com>
2023-02-07 21:08:44 +0100

    add script create_venv

---
+++ create_venv.sh
@@ -0,0 +1,15 @@
+rm -r ./pyenv
+python3 -m venv pyenv
+source ./pyenv/bin/activate
+python -m pip install -U pip
+python -m pip install pylint
+python -m pip install black
+python -m pip install pyserial
+python -m pip install tornado
+python -m pip install python-socketio
+python -m pip install transcrypt
+python -m pip install watchdog
+cd audio_controller
+python -m pip install --editable .
+cd ..
+deactivate
